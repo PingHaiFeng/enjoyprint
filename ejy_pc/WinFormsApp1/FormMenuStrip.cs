ff9d4e415885aba7198a12bd68fd436a3b4b52c2
fangxaing <920988322@qq.com>
2022-04-18 18:33:28 +0800

    #

--- ejy_pc/WinFormsApp1/FormMenuStrip.cs
+++ ejy_pc/WinFormsApp1/FormMenuStrip.cs
@@ -4,7 +4,9 @@ using System.ComponentModel;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 using WinFormsApp1;
 
@@ -19,15 +21,41 @@ namespace CloudPrint
 
         private void button1_Click(object sender, EventArgs e)
         {
-            FormAdminLock formAdminLock = new FormAdminLock();
-            formAdminLock.Show();
-            Hide();
-        }
+            string fileFullPath = "data.bin";
+            if (File.Exists(fileFullPath))
+            {
 
+                File.Delete(fileFullPath);
+                
+                    Application.ExitThread();
+                    Thread thtmp = new Thread(new ParameterizedThreadStart(run));
+                    object appName = Application.ExecutablePath;
+                    Thread.Sleep(1);
+                    thtmp.Start(appName);
+                
+
+            }
+        }
+        private void Restart()
+        {
+            Thread thtmp = new Thread(new ParameterizedThreadStart(run));
+            object appName = Application.ExecutablePath;
+            Thread.Sleep(200);
+            thtmp.Start(appName);
+        }
+        private void run(Object obj)
+        {
+            Process ps = new Process();
+            ps.StartInfo.FileName = obj.ToString();
+            ps.Start();
+        }
+    
         private void button2_Click(object sender, EventArgs e)
         {
             try
             {
+                //FormAdminLock formAdminLock = new FormAdminLock();
+                //formAdminLock.Show();
                 var psi = new ProcessStartInfo
                 {
                     FileName = Config.ADMINURL,
@@ -50,6 +78,13 @@ namespace CloudPrint
         private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FormAdminLock formAdminLock = new FormAdminLock();
+            formAdminLock.Show();
+            Hide();
         }
     }
 }
